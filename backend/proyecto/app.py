dd1050c35d8d50841cb544d28b0d3785082827d5
daniDev <danielvillalbac97@gmail.com>
2023-12-14 07:10:27 -0300

    Actualizacion del Readme y el back

--- backend/proyecto/app.py
+++ backend/proyecto/app.py
@@ -8,6 +8,8 @@ CORS(app) #modulo cors es para que me permita acceder desde el frontend al backe
 
 
 # configuro la base de datos, con el nombre el usuario y la clave
+
+#
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root@127.0.0.1/proyecto'
 # URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
